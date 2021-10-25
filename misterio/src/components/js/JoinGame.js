c7f8ab67e403c86f8bc2232def5f05afd807df61
sarakim1309 <victoriasarakim.sk@gmail.com>
2021-10-24 21:42:50 -0300

    remains connections with end points

---
+++ misterio/src/components/js/JoinGame.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../css/Button.css";
+
+import {useHistory} from "react-router-dom";
+
+export default function JoinGame() { 
+	let history = useHistory();
+
+	const handleClick = () => {
+		history.push("/JoinForm");
+	}
+
+	return (
+		<div>
+			<button className = "continue" onClick= {handleClick}> Unirse a Partida </button>
+		</div>
+	);
+}
